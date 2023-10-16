61109c38d2265b1ee649fc3e3bffcfcfe17d40eb
Varun <vvijayababu4334@gmail.com>
2023-10-15 19:54:05 -0700

    broken tabs and working todo

--- Style.js
+++ Style.js
@@ -1,10 +1,64 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const styles = StyleSheet.create({ 
+	container: { 
+		flex: 1, 
+		padding: 40, 
+		marginTop: 40, 
+	}, 
+	title: { 
+		fontSize: 24, 
+		fontWeight: "bold", 
+		marginBottom: 20, 
+	}, 
+	heading: { 
+		fontSize: 30, 
+		fontWeight: "bold", 
+		marginBottom: 7, 
+		color: "green", 
+	}, 
+	input: { 
+		borderWidth: 3, 
+		borderColor: "#ccc", 
+		padding: 10, 
+		marginBottom: 10, 
+		borderRadius: 10, 
+		fontSize: 18, 
+	}, 
+	addButton: { 
+		backgroundColor: "green", 
+		padding: 10, 
+		borderRadius: 5, 
+		marginBottom: 10, 
+	}, 
+	addButtonText: { 
+		color: "white", 
+		fontWeight: "bold", 
+		textAlign: "center", 
+		fontSize: 18, 
+	}, 
+	task: { 
+		flexDirection: "row", 
+		justifyContent: "space-between", 
+		alignItems: "center", 
+		marginBottom: 15, 
+		fontSize: 18, 
+	}, 
+	itemList: { 
+		fontSize: 19, 
+	}, 
+	taskButtons: { 
+		flexDirection: "row", 
+	}, 
+	editButton: { 
+		marginRight: 10, 
+		color: "green", 
+		fontWeight: "bold", 
+		fontSize: 18, 
+	}, 
+	deleteButton: { 
+		color: "red", 
+		fontWeight: "bold", 
+		fontSize: 18, 
+	}, 
+}); 
