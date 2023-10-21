65a0f85c4bc35a44ecbc56bbd177d89497e5e72c
Varun <vvijayababu4334@gmail.com>
2023-10-20 17:22:03 -0700

    added purchase and todo tabs

--- App.js
+++ App.js
@@ -10,125 +10,23 @@ import {
 	StyleSheet, 
 } from "react-native"; 
 import {styles} from "./Style.js";
+import {TodoScreen} from "./TodoTab.js";
+import { PayScreen } from "./PayTab.js";
+
 const Tab = createMaterialTopTabNavigator();
+const tabs = [{name: "Todo", component: TodoScreen}, {name: "Purchases", component: PayScreen}];
 
 const App = () => { 
-  
-	const [task, setTask] = useState(""); 
-	const [tasks, setTasks] = useState([]); 
-	const [editIndex, setEditIndex] = useState(-1); 
-  const [tabs, setTabs] = React.useState([
-    {
-      name : "tab-1",
-      component : TabScreen,
-    }
-])
-
-
-const addNewTab = () => {
-  setTabs(tabs => [
-    ...tabs,
-    {
-      name : "tab-" + (parseInt(tabs.pop().name.replace("tab-", "")) + 1),
-      component : TabScreen,
-    }
-  ])
-}
-
-
-const remove = (route) => {
-  setTabs(tabs => tabs.filter(tab => tab.name !== route.name))
-}
-
-
-function TabScreen({route}) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text onPress={addNewTab}>Add New Tab</Text>
-      {
-        route.name !== "tab-1" && <Text onPress={() => remove(route)}>close me</Text>
-      } 
-    </View>
-  );
-}
-
-	const handleAddTask = () => { 
-		if (task) { 
-			if (editIndex !== -1) { 
-				// Edit existing task 
-				const updatedTasks = [...tasks]; 
-				updatedTasks[editIndex] = task; 
-				setTasks(updatedTasks); 
-				setEditIndex(-1); 
-			} else { 
-				// Add new task 
-				setTasks([...tasks, task]); 
-			} 
-			setTask(""); 
-		} 
-	}; 
-
-	const handleEditTask = (index) => { 
-		const taskToEdit = tasks[index]; 
-		setTask(taskToEdit); 
-		setEditIndex(index); 
-	}; 
-
-	const handleDeleteTask = (index) => { 
-		const updatedTasks = [...tasks]; 
-		updatedTasks.splice(index, 1); 
-		setTasks(updatedTasks); 
-	}; 
-
-	const renderItem = ({ item, index }) => ( 
-		<View style={styles.task}> 
-			<Text 
-				style={styles.itemList}>{item}</Text> 
-			<View 
-				style={styles.taskButtons}> 
-				<TouchableOpacity 
-					onPress={() => handleEditTask(index)}> 
-					<Text 
-						style={styles.editButton}>Edit</Text> 
-				</TouchableOpacity> 
-				<TouchableOpacity 
-					onPress={() => handleDeleteTask(index)}> 
-					<Text 
-						style={styles.deleteButton}>Delete</Text> 
-				</TouchableOpacity> 
-			</View> 
-		</View> 
-	); 
-
 	return ( 
 		<View style={styles.container}> 
 			<Text style={styles.heading}>Roomie</Text> 
-			<Text style={styles.title}>ToDo Page</Text> 
-      <NavigationContainer>
+			<NavigationContainer>
     <Tab.Navigator>
       {
         tabs.map(tab => <Tab.Screen key={tab.name} name={tab.name} component={tab.component} />)
       }
     </Tab.Navigator>
   </NavigationContainer>
-			<TextInput 
-				style={styles.input} 
-				placeholder="Enter task"
-				value={task} 
-				onChangeText={(text) => setTask(text)} 
-			/> 
-			<TouchableOpacity 
-				style={styles.addButton} 
-				onPress={handleAddTask}> 
-				<Text style={styles.addButtonText}> 
-					{editIndex !== -1 ? "Update Task" : "Add Task"} 
-				</Text> 
-			</TouchableOpacity> 
-			<FlatList 
-				data={tasks} 
-				renderItem={renderItem} 
-				keyExtractor={(item, index) => index.toString()} 
-			/> 
 		</View> 
 	); 
 }; 
