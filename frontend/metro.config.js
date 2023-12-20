986c44b0b51536c89d58e31e5e7d05fde0373814
Varun <vvijayababu4334@gmail.com>
2023-12-19 23:24:35 -0800

    metroconfig2

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@expo/metro-config');
+const {getDefaultConfig} = require('@expo/metro-config');
 
 /**
  * Metro configuration
@@ -8,4 +8,4 @@ const {getDefaultConfig, mergeConfig} = require('@expo/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = getDefaultConfig(__dirname);
